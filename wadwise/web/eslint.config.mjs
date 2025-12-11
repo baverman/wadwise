a1b0766bbe3b2e5328e6b71f3a77bb7f34e2d575
Anton Bobrov <baverman@gmail.com>
2025-12-11 12:25:07 +0000

    add import checks to eslint

--- wadwise/web/eslint.config.mjs
+++ wadwise/web/eslint.config.mjs
@@ -1,13 +1,14 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
+import pluginImport from 'eslint-plugin-import'
 
 export default defineConfig([
     {
         files: ['src/**/*.{js,mjs,cjs}'],
         plugins: { js },
-        extends: ['js/recommended'],
-        languageOptions: { globals: globals.browser },
+        extends: ['js/recommended', pluginImport.flatConfigs.recommended],
+        languageOptions: { globals: globals.browser, ecmaVersion: 'latest' },
         rules: { 'no-unused-vars': 'warn' },
     },
 ])
